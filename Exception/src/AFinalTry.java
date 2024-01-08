193a1faab83b517760044f87180cd1a3b7335164
sotaMiracle <kiennguyen16998@gmail.com>
2024-01-08 08:36:09 +0700

    end exception

--- Exception/src/AFinalTry.java
+++ Exception/src/AFinalTry.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class AFinalTry {
+
+import java.io.IOException;
+
+public class AFinalTry {
+    public static void main(String[] args) {
+        someMethodThrowsException();
+    }
+
+    public static void someMethodThrowsException() {
+        try {
+            System.out.println("Doing something");
+        } catch (Exception e) {
+            throw new IOException("Testing catch");
+        } finally {
+            throw new RuntimeException("Testing finally");
+        }
+
+    }
 }
